6b92366e3226ef18a1166a7bc7a7c228c27942be
szczepanbarszczowski <szczepan.barszczowski@gmail.com>
2019-06-27 12:13:23 +0200

    Added query params support

--- src/utils/locationFromUrl.js
+++ src/utils/locationFromUrl.js
@@ -3,10 +3,19 @@ import { parse, format } from 'url'
 const locationFromUrl = (_url) => {
   const url = typeof _url === 'object' ? format(_url) : _url
   const { hash, search, pathname } = parse(url)
+  let query = {}
+
+  const str = typeof search === 'string' ? search.substring(1) : ''
+
+  str.replace(/([^=&]+)=([^&]*)/g, function(m, key, value) {
+    query[decodeURIComponent(key)] = decodeURIComponent(value)
+  })
+
   return {
     pathname,
     search: search ? search : '',
     hash: hash ? hash : '',
+    query
   }
 }
 
